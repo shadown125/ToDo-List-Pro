93e59127c21633ebab85a7c42c73c8d87e6cc983
Dawid Oleksiuk <shadyolown13@onet.pl>
2020-04-22 19:30:07 +0200

    Add new svg, Add delete button to Popup, Add functionality to delete button, Add style to Popup, Add new structure to Popup

--- JavaScript/Popup.js
+++ JavaScript/Popup.js
@@ -4,13 +4,20 @@ class Popup {
         const button = document.querySelector('.task-add-button');
         const popup = document.querySelector('.popup-task');
         const popupContent = document.querySelector('.popup-task__content');
+        const popupContentButton = document.querySelector('.popup-task__svg-button');
 
         const displayPopup = () => {
             popup.style.visibility = "visible";
             popupContent.style.visibility = "visible";
         }
 
+        const hidePopup = () => {
+            popup.style.visibility = "hidden";
+            popupContent.style.visibility = "hidden";
+        }
+
         button.addEventListener('click', displayPopup);
+        popupContentButton.addEventListener('click', hidePopup);
     }
     
 }
