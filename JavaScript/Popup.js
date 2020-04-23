9a14f4201f2a28f4f67ed55b3c6766d695d310e9
Dawid Oleksiuk <shadyolown13@onet.pl>
2020-04-23 13:32:38 +0200

    Modyficate html structure, Rebuild popup from JS to HTML&CSS, Popup has now new effects

--- JavaScript/Popup.js
+++ JavaScript/Popup.js
@@ -1,25 +1,23 @@
 // Popup
 class Popup {
     constructor() {
-        const button = document.querySelector('.task-add-button');
-        const popup = document.querySelector('.popup-task');
-        const popupContent = document.querySelector('.popup-task__content');
-        const popupContentButton = document.querySelector('.popup-task__svg-button');
+        // const button = document.querySelector('.task-add-button');
+        // const popup = document.querySelector('.popup-task');
+        // const popupContent = document.querySelector('.popup-task__content');
+        // const popupContentButton = document.querySelector('.popup-task__svg-button');
 
-        const displayPopup = (e) => {
-            e.preventDefault();
-            popup.style.visibility = "visible";
-            popupContent.style.visibility = "visible";
-        }
+        // const displayPopup = (e) => {
+        //     popup.style.visibility = "visible";
+        //     popupContent.style.visibility = "visible";
+        // }
 
-        const hidePopup = (e) => {
-            e.preventDefault();
-            popup.style.visibility = "hidden";
-            popupContent.style.visibility = "hidden";
-        }
+        // const hidePopup = (e) => {
+        //     popup.style.visibility = "hidden";
+        //     popupContent.style.visibility = "hidden";
+        // }
 
-        button.addEventListener('click', displayPopup);
-        popupContentButton.addEventListener('click', hidePopup);
+        // button.addEventListener('click', displayPopup);
+        // popupContentButton.addEventListener('click', hidePopup);
     }
     
 }
