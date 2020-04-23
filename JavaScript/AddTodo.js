94244c4e6bcf9a0df9a540b0de9dd2b51ab493ec
Dawid Oleksiuk <shadyolown13@onet.pl>
2020-04-23 15:50:34 +0200

    Add new button to popup and new JS class

---
+++ JavaScript/AddTodo.js
@@ -0,0 +1,19 @@
+class AddTodo {
+    constructor() {
+
+        const inputTitle = document.querySelector('.popup-task__title'); 
+        const inputDescription = document.querySelector('.popup-task__description');
+        const todoContainer = document.querySelector('.todos');
+        const addTaskButton = document.querySelector('.popup-task__button');
+
+        const addTask = (e) => {
+
+        }
+
+        const removeTask = (e) => {
+
+        }
+
+    }
+
+}
--- JavaScript/Popup.js
+++ JavaScript/Popup.js
@@ -18,6 +18,7 @@ class Popup {
 
         // button.addEventListener('click', displayPopup);
         // popupContentButton.addEventListener('click', hidePopup);
+       
     }
     
 }
