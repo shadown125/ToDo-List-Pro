d0011177941891203ff00983f6bf414b20e9b9a2
Dawid Oleksiuk <shadyolown13@onet.pl>
2020-05-11 15:12:10 +0200

    Add new functionality to first option in options, Add new html structure for new option

--- JavaScript/Options.js
+++ JavaScript/Options.js
@@ -1,13 +1,32 @@
 class Options {
     constructor() {
-        const messegesContainer = document.querySelector('.icons__messeges-wrapper ')
-        const messegesIcon = document.querySelector('.icons__single-item-2')
-        const body = document.getElementById('body')
+        const messegesContainer = document.querySelector('.icons__messeges-wrapper ');
+        const messegesIcon = document.querySelector('.icons__single-item-2');
+        const body = document.getElementById('body');
+
+        // Option List buttons
+        const optionList = document.getElementsByClassName('popup-settings__options-list');
+        const nameButton = document.querySelector('.popup-settings__option-name')
+        const professionButton = document.querySelector('.popup-settings__option-profession')
+        const imageButton = document.querySelector('.popup-settings__option-image')
+        const languageButton = document.querySelector('.popup-settings__option-Language')
+        const deleteButton = document.querySelector('.popup-settings__option-delete')
+        const logOutButton = document.querySelector('.popup-settings__option-log-out')
+        
+        // Changing options
+        const nameContainer = document.querySelector('.name-change');
+
+        // Buttons
+        const buttonBack = document.querySelector('.name-change__button-back');
+        const buttonApply = document.querySelector('.name-change__button-apply');
+        const ahrefPopup = document.querySelector('.popup-settings__svg-ahref')
+
 
         let flag = 0;
         let flag2 = false;
 
         const showMesseges = () => {
+            flag = 0;
 
             if(messegesContainer.style.display === "block") {
                 messegesContainer.style.display = 'none'
@@ -33,9 +52,29 @@ class Options {
             }
         }
 
+        const showChangeName = () => {
+
+            for (let i=0; i< optionList.length; i++) {
+                optionList[i].style.display = 'none';
+            }
+            
+            nameContainer.style.display = 'block';
+        }
+
+        const backToOptions = () => {
+            for (let i=0; i< optionList.length; i++) {
+                optionList[i].style.display = 'block';
+            }
+            
+            nameContainer.style.display = 'none';
+        }
+
+
         messegesContainer.addEventListener('click', preventBug);
         messegesIcon.addEventListener('click', showMesseges)
         body.addEventListener('click', hidePopup);
-        
+        nameButton.addEventListener('click', showChangeName);
+        buttonBack.addEventListener('click', backToOptions)
+        ahrefPopup.addEventListener('click', backToOptions)
     }
 }
