14b231ece0e5c65392a01eabd38f890a4dac469d
Dawid Oleksiuk <shadyolown13@onet.pl>
2020-05-31 13:35:42 +0200

    Rewrite colors section, Add jquery to color section, Add jquery to progress section, Maintain JS code in progress section

--- JavaScript/Colors.js
+++ JavaScript/Colors.js
@@ -1,70 +1,257 @@
 class Colors {
     constructor() {
-        this.green = document.querySelector('.color-green');
-        this.blue = document.querySelector('.color-blue');
-        this.violet = document.querySelector('.color-violet');
-        this.orange = document.querySelector('.color-orange');
-        this.main = document.querySelector('.color-main');
+        this.$green = $('.color-green');
+        this.$blue = $('.color-blue');
+        this.$violet = $('.color-violet');
+        this.$orange = $('.color-orange');
+        this.$main = $('.color-main');
 
 
-        this.container = document.querySelector('.container');
-        this.sidebar = document.querySelector('.sidebar');
-        this.todo = document.querySelector('.todos');
-        this.progressContent = document.querySelector('.progression');
-        this.interfaceContainer = document.querySelector('.int-container');
-        this.header = document.querySelector('.header');
+        this.$container = $('.container');
+        this.$sidebar = $('.sidebar');
+        this.$todo = $('.todos');
+        this.$progressContent = $('.progression');
+        this.$interfaceContainer = $('.int-container');
+        this.$header = $('.header');
 
 
         
-        this.main.addEventListener('click', this.mainFilter.bind(this))
-        this.orange.addEventListener('click', this.orangeFilter.bind(this))
-        this.violet.addEventListener('click', this.violetFilter.bind(this))
-        this.blue.addEventListener('click', this.blueFilter.bind(this))
-        this.green.addEventListener('click', this.greenFilter.bind(this))
+        this.$main.on('click', this.mainFilter.bind(this))
+        this.$orange.on('click', this.orangeFilter.bind(this))
+        this.$violet.on('click', this.violetFilter.bind(this))
+        this.$blue.on('click', this.blueFilter.bind(this))
+        this.$green.on('click', this.greenFilter.bind(this))
     }
 
     mainFilter() {
-        this.header.style.backgroundImage = 'linear-gradient(#2E3553, #2E3553)';
-        this.container.style.backgroundImage = 'linear-gradient(#2a314f, #2E3553)';
-        this.sidebar.style.backgroundImage = 'linear-gradient(to right bottom, #2E3553, #4C4177)';
-        this.progressContent.style.backgroundImage = 'linear-gradient(#4C4177, #4C4177)';
-        this.todo.style.backgroundImage = 'linear-gradient(#4C4177, #4C4177)';
-        this.interfaceContainer.style.backgroundImage = 'linear-gradient(#4C4177, #4C4177)';
+
+        if(this.$header.hasClass('orange-header')) {
+
+            this.$header.removeClass('orange-header');
+            this.$container.removeClass('orange-container');
+            this.$sidebar.removeClass('orange-sidebar');
+            this.$progressContent.removeClass('orange-progress');
+            this.$todo.removeClass('orange-todo');
+            this.$interfaceContainer.removeClass('orange-interface');
+
+        } else if (this.$header.hasClass('green-header')) {
+
+            this.$header.removeClass('green-header');
+            this.$container.removeClass('green-container');
+            this.$sidebar.removeClass('green-sidebar');
+            this.$progressContent.removeClass('green-progress');
+            this.$todo.removeClass('green-todo');
+            this.$interfaceContainer.removeClass('green-interface');
+
+        } else if(this.$header.hasClass('violet-header')) {
+
+            this.$header.removeClass('violet-header');
+            this.$container.removeClass('violet-container');
+            this.$sidebar.removeClass('violet-sidebar');
+            this.$progressContent.removeClass('violet-progress');
+            this.$todo.removeClass('violet-todo');
+            this.$interfaceContainer.removeClass('violet-interface');
+
+        } else if (this.$header.hasClass('blue-header')) {
+
+            this.$header.removeClass('blue-header');
+            this.$container.removeClass('blue-container');
+            this.$sidebar.removeClass('blue-sidebar');
+            this.$progressContent.removeClass('blue-progress');
+            this.$todo.removeClass('blue-todo');
+            this.$interfaceContainer.removeClass('blue-interface');
+        }
+
+        this.$header.addClass('main-header');
+        this.$container.addClass('main-container');
+        this.$sidebar.addClass('main-sidebar');
+        this.$progressContent.addClass('main-progress');
+        this.$todo.addClass('main-todo');
+        this.$interfaceContainer.addClass('main-interface');
     }
 
     violetFilter() {
-        this.header.style.backgroundImage = 'linear-gradient(to right bottom, #2e3553, #2d3452, #2d3351, #2c3351, #2b3250, #2d3353, #2f3456, #313559, #373861, #3d3b68, #443e70, #4c4177)';
-        this.container.style.backgroundImage = 'linear-gradient(to right bottom, #2e3553, #32385a, #373a61, #3d3c68, #433e6f, #433e6e, #423d6e, #423d6d, #3b3a66, #35375e, #2f3457, #2a314f)';
-        this.sidebar.style.backgroundImage = 'linear-gradient(to right bottom, #2e3553, #32385a, #373a61, #3d3c68, #433e6f, #433e6e, #423d6e, #423d6d, #3b3a66, #35375e, #2f3457, #2a314f)';
-        this.progressContent.style.backgroundImage = 'linear-gradient(to right bottom, #2e3553, #2d3452, #2d3351, #2c3351, #2b3250, #2d3353, #2f3456, #313559, #373861, #3d3b68, #443e70, #4c4177)';
-        this.todo.style.backgroundImage = 'linear-gradient(to bottom, #2e3553, #2d3452, #2d3351, #2c3351, #2b3250, #2d3353, #2f3456, #313559, #373861, #3d3b68, #443e70, #4c4177)';
-        this.interfaceContainer.style.backgroundImage = 'linear-gradient(to right top, #2e3553, #32385a, #373a61, #3d3c68, #433e6f, #433e6e, #423d6e, #423d6d, #3b3a66, #35375e, #2f3457, #2a314f)';
+        
+        if(this.$header.hasClass('orange-header')) {
+
+            this.$header.removeClass('orange-header');
+            this.$container.removeClass('orange-container');
+            this.$sidebar.removeClass('orange-sidebar');
+            this.$progressContent.removeClass('orange-progress');
+            this.$todo.removeClass('orange-todo');
+            this.$interfaceContainer.removeClass('orange-interface');
+
+        } else if (this.$header.hasClass('green-header')) {
+
+            this.$header.removeClass('green-header');
+            this.$container.removeClass('green-container');
+            this.$sidebar.removeClass('green-sidebar');
+            this.$progressContent.removeClass('green-progress');
+            this.$todo.removeClass('green-todo');
+            this.$interfaceContainer.removeClass('green-interface');
+
+        } else if (this.$header.hasClass('main-header')) {
+
+            this.$header.removeClass('main-header');
+            this.$container.removeClass('main-container');
+            this.$sidebar.removeClass('main-sidebar');
+            this.$progressContent.removeClass('main-progress');
+            this.$todo.removeClass('main-todo');
+            this.$interfaceContainer.removeClass('main-interface');
+
+        } else if (this.$header.hasClass('blue-header')) {
+
+            this.$header.removeClass('blue-header');
+            this.$container.removeClass('blue-container');
+            this.$sidebar.removeClass('blue-sidebar');
+            this.$progressContent.removeClass('blue-progress');
+            this.$todo.removeClass('blue-todo');
+            this.$interfaceContainer.removeClass('blue-interface');
+        }
+
+        this.$header.addClass('violet-header');
+        this.$container.addClass('violet-container');
+        this.$sidebar.addClass('violet-sidebar');
+        this.$progressContent.addClass('violet-progress');
+        this.$todo.addClass('violet-todo');
+        this.$interfaceContainer.addClass('violet-interface');
     }
 
     greenFilter() {
-        this.header.style.backgroundImage = 'linear-gradient(to left, #21777d, #21747c, #22717a, #236e79, #246b77, #256875, #266673, #276371, #28606f, #295d6c, #2a5a6a, #2b5767)';
-        this.container.style.backgroundImage = 'linear-gradient(to left top, #21777d, #21747c, #22717a, #236e79, #246b77, #256875, #266673, #276371, #28606f, #295d6c, #2a5a6a, #2b5767)';
-        this.sidebar.style.backgroundImage = 'linear-gradient(to left top, #268782, #248481, #228181, #217e80, #217b7f, #21787e, #21767c, #21737b, #227079, #236d78, #246a76, #256774)';
-        this.progressContent.style.backgroundImage = 'linear-gradient(to left top, #268782, #248481, #228181, #217e80, #217b7f, #21787e, #21767c, #21737b, #227079, #236d78, #246a76, #256774)';
-        this.todo.style.backgroundImage = 'linear-gradient(to top, #268782, #248481, #228181, #217e80, #217b7f, #21787e, #21767c, #21737b, #227079, #236d78, #246a76, #256774)';
-        this.interfaceContainer.style.backgroundImage = 'linear-gradient(to right top, #268782, #248481, #228181, #217e80, #217b7f, #21787e, #21767c, #21737b, #227079, #236d78, #246a76, #256774)';
+
+        if(this.$header.hasClass('orange-header')) {
+            this.$header.removeClass('orange-header');
+            this.$container.removeClass('orange-container');
+            this.$sidebar.removeClass('orange-sidebar');
+            this.$progressContent.removeClass('orange-progress');
+            this.$todo.removeClass('orange-todo');
+            this.$interfaceContainer.removeClass('orange-interface');
+
+        } else if(this.$header.hasClass('violet-header')) {
+
+            this.$header.removeClass('violet-header');
+            this.$container.removeClass('violet-container');
+            this.$sidebar.removeClass('violet-sidebar');
+            this.$progressContent.removeClass('violet-progress');
+            this.$todo.removeClass('violet-todo');
+            this.$interfaceContainer.removeClass('violet-interface');
+
+        } else if (this.$header.hasClass('main-header')) {
+
+            this.$header.removeClass('main-header');
+            this.$container.removeClass('main-container');
+            this.$sidebar.removeClass('main-sidebar');
+            this.$progressContent.removeClass('main-progress');
+            this.$todo.removeClass('main-todo');
+            this.$interfaceContainer.removeClass('main-interface');
+
+        } else if (this.$header.hasClass('blue-header')) {
+
+            this.$header.removeClass('blue-header');
+            this.$container.removeClass('blue-container');
+            this.$sidebar.removeClass('blue-sidebar');
+            this.$progressContent.removeClass('blue-progress');
+            this.$todo.removeClass('blue-todo');
+            this.$interfaceContainer.removeClass('blue-interface');
+        }
+
+        this.$header.addClass('green-header');
+        this.$container.addClass('green-container');
+        this.$sidebar.addClass('green-sidebar');
+        this.$progressContent.addClass('green-progress');
+        this.$todo.addClass('green-todo');
+        this.$interfaceContainer.addClass('green-interface');
     }
 
     orangeFilter() {
-        this.header.style.backgroundImage = 'linear-gradient(to right, #f94327, #f83a36, #f63343, #f22d4f, #ed2a5a, #e82a63, #e12b6c, #da2e74, #d1327c, #c73783, #bc3b89, #b1408e)';
-        this.container.style.backgroundImage = 'linear-gradient(to right bottom, #d04072, #ce417a, #ca4382, #c7468a, #c24991, #bb4e9b, #b353a4, #a958ac, #995fb7, #8667c0, #6f6dc6, #5573ca)';
-        this.sidebar.style.backgroundImage = 'linear-gradient(to right bottom, #d14545, #cf424d, #cb4055, #c73e5d, #c23e64, #bd3f69, #b7406e, #b14173, #a94378, #a1447c, #99467f, #904882)';
-        this.progressContent.style.backgroundImage = 'linear-gradient(to right bottom, #d14545, #cf424d, #cb4055, #c73e5d, #c23e64, #bd3f69, #b7406e, #b14173, #a94378, #a1447c, #99467f, #904882)';
-        this.todo.style.backgroundImage = 'linear-gradient(to bottom, #d14545, #cf424d, #cb4055, #c73e5d, #c23e64, #bd3f69, #b7406e, #b14173, #a94378, #a1447c, #99467f, #904882)';
-        this.interfaceContainer.style.backgroundImage = 'linear-gradient(to right top, #d14545, #cf424d, #cb4055, #c73e5d, #c23e64, #bd3f69, #b7406e, #b14173, #a94378, #a1447c, #99467f, #904882)';
+
+        if(this.$header.hasClass('green-header')) {
+            this.$header.removeClass('green-header');
+            this.$container.removeClass('green-container');
+            this.$sidebar.removeClass('green-sidebar');
+            this.$progressContent.removeClass('green-progress');
+            this.$todo.removeClass('green-todo');
+            this.$interfaceContainer.removeClass('green-interface');
+
+        } else if(this.$header.hasClass('violet-header')) {
+
+            this.$header.removeClass('violet-header');
+            this.$container.removeClass('violet-container');
+            this.$sidebar.removeClass('violet-sidebar');
+            this.$progressContent.removeClass('violet-progress');
+            this.$todo.removeClass('violet-todo');
+            this.$interfaceContainer.removeClass('violet-interface');
+            
+        } else if (this.$header.hasClass('main-header')) {
+
+            this.$header.removeClass('main-header');
+            this.$container.removeClass('main-container');
+            this.$sidebar.removeClass('main-sidebar');
+            this.$progressContent.removeClass('main-progress');
+            this.$todo.removeClass('main-todo');
+            this.$interfaceContainer.removeClass('main-interface');
+
+        } else if (this.$header.hasClass('blue-header')) {
+
+            this.$header.removeClass('blue-header');
+            this.$container.removeClass('blue-container');
+            this.$sidebar.removeClass('blue-sidebar');
+            this.$progressContent.removeClass('blue-progress');
+            this.$todo.removeClass('blue-todo');
+            this.$interfaceContainer.removeClass('blue-interface');
+        }
+
+        this.$header.addClass('orange-header');
+        this.$container.addClass('orange-container');
+        this.$sidebar.addClass('orange-sidebar');
+        this.$progressContent.addClass('orange-progress');
+        this.$todo.addClass('orange-todo');
+        this.$interfaceContainer.addClass('orange-interface');
     }
 
     blueFilter() {
-        this.header.style.backgroundImage = 'linear-gradient(to right, #8c6cc3, #8672c9, #8077ce, #7a7dd3, #7382d8, #6c87dc, #658be0, #5e90e3, #5695e6, #4d9ae8, #449eea, #3ba3ec)';
-        this.container.style.backgroundImage = 'linear-gradient(to right bottom, #8c6cc3, #8672c9, #8077ce, #7a7dd3, #7382d8, #6c87dc, #658be0, #5e90e3, #5695e6, #4d9ae8, #449eea, #3ba3ec)';
-        this.sidebar.style.backgroundImage = 'linear-gradient(to right bottom, #6989de, #618ee2, #5993e5, #5098e8, #479dea, #3fa1ec, #37a5ed, #2ea9ee, #23adef, #18b2ef, #0cb6ef, #01baef)';
-        this.progressContent.style.backgroundImage = 'linear-gradient(to right bottom, #6989de, #618ee2, #5993e5, #5098e8, #479dea, #3fa1ec, #37a5ed, #2ea9ee, #23adef, #18b2ef, #0cb6ef, #01baef)';
-        this.todo.style.backgroundImage = 'linear-gradient(to bottom, #6989de, #618ee2, #5993e5, #5098e8, #479dea, #3fa1ec, #37a5ed, #2ea9ee, #23adef, #18b2ef, #0cb6ef, #01baef)';
-        this.interfaceContainer.style.backgroundImage = 'linear-gradient(to right top, #6989de, #618ee2, #5993e5, #5098e8, #479dea, #3fa1ec, #37a5ed, #2ea9ee, #23adef, #18b2ef, #0cb6ef, #01baef)';
+
+        if(this.$header.hasClass('green-header')) {
+            this.$header.removeClass('green-header');
+            this.$container.removeClass('green-container');
+            this.$sidebar.removeClass('green-sidebar');
+            this.$progressContent.removeClass('green-progress');
+            this.$todo.removeClass('green-todo');
+            this.$interfaceContainer.removeClass('green-interface');
+
+        } else if(this.$header.hasClass('violet-header')) {
+
+            this.$header.removeClass('violet-header');
+            this.$container.removeClass('violet-container');
+            this.$sidebar.removeClass('violet-sidebar');
+            this.$progressContent.removeClass('violet-progress');
+            this.$todo.removeClass('violet-todo');
+            this.$interfaceContainer.removeClass('violet-interface');
+            
+        } else if (this.$header.hasClass('main-header')) {
+
+            this.$header.removeClass('main-header');
+            this.$container.removeClass('main-container');
+            this.$sidebar.removeClass('main-sidebar');
+            this.$progressContent.removeClass('main-progress');
+            this.$todo.removeClass('main-todo');
+            this.$interfaceContainer.removeClass('main-interface');
+
+        } else if (this.$header.hasClass('orange-header')) {
+
+            this.$header.removeClass('orange-header');
+            this.$container.removeClass('orange-container');
+            this.$sidebar.removeClass('orange-sidebar');
+            this.$progressContent.removeClass('orange-progress');
+            this.$todo.removeClass('orange-todo');
+            this.$interfaceContainer.removeClass('orange-interface');
+        }
+
+        this.$header.addClass('blue-header');
+        this.$container.addClass('blue-container');
+        this.$sidebar.addClass('blue-sidebar');
+        this.$progressContent.addClass('blue-progress');
+        this.$todo.addClass('blue-todo');
+        this.$interfaceContainer.addClass('blue-interface');
     }
 }
--- JavaScript/Progress.js
+++ JavaScript/Progress.js
@@ -1,65 +1,71 @@
 class Progress {
     constructor() {
-        const dateNumber = document.querySelector('.progression__day');
-        const monthDate = document.querySelector('.progression__month');
-        const progressLine = document.querySelector('.progression__bar');
-        const doneTodoCounter = document.querySelector('.progression__done-counter')
-        const mustDoCounter = document.querySelector('.progression__must-do-task-counter')
+        this.$dateNumber = $('.progression__day');
+        this.$monthDate = $('.progression__month');
+        this.$progressLine = $('.progression__bar');
+        this.$doneTodoCounter = $('.progression__done-counter')
+        this.$mustDoCounter = $('.progression__must-do-task-counter')
         
 
 
         // DATE
-        setInterval(() => {
-
-        const nowTime = new Date().getDate();
-        const nowMonth = new Date().getMonth();
-
-        dateNumber.textContent = nowTime;
-
-        if(nowMonth === 0) {
-            monthDate.textContent = 'JAN'
-        } else if (nowMonth === 1) {
-            monthDate.textContent = 'FEB'
-        } else if (nowMonth === 2) {
-            monthDate.textContent = 'MARCH'
-        } else if (nowMonth === 3) {
-            monthDate.textContent = 'APR'
-        } else if (nowMonth === 4) {
-            monthDate.textContent = 'MAY'
-        } else if (nowMonth === 5) {
-            monthDate.textContent = 'JUN'
-        } else if (nowMonth === 6) {
-            monthDate.textContent = 'JUL'
-        } else if (nowMonth === 7) {
-            monthDate.textContent = 'AUG'
-        } else if (nowMonth === 8) {
-            monthDate.textContent = 'SEPT'
-        } else if (nowMonth === 9) {
-            monthDate.textContent = 'OCT'
-        } else if (nowMonth === 10) {
-            monthDate.textContent = 'NOV'
-        } else if (nowMonth === 11) {
-            monthDate.textContent = 'DEC'
-        }
-
-        }, 100)
         
+        this.months();
 
         // PROGRESSION LINE
 
-        
+        this.progressLineContent();
+   
+    }
+
+    months() {
+        setInterval(() => {
+
+            const nowTime = new Date().getDate();
+            const nowMonth = new Date().getMonth();
+    
+            this.$dateNumber.text(nowTime);
+    
+            if(nowMonth === 0) {
+                this.$monthDate.text('JAN')
+            } else if (nowMonth === 1) {
+                this.$monthDate.text('FEB')
+            } else if (nowMonth === 2) {
+                this.$monthDate.text('MARCH')
+            } else if (nowMonth === 3) {
+                this.$monthDate.text('APR')
+            } else if (nowMonth === 4) {
+                this.$monthDate.text('MAY')
+            } else if (nowMonth === 5) {
+                this.$monthDate.text('JUN')
+            } else if (nowMonth === 6) {
+                this.$monthDate.text('JUL')
+            } else if (nowMonth === 7) {
+                this.$monthDate.text('AUG')
+            } else if (nowMonth === 8) {
+                this.$monthDate.text('SEPT')
+            } else if (nowMonth === 9) {
+                this.$monthDate.text('OCT')
+            } else if (nowMonth === 10) {
+                this.$monthDate.text('NOV')
+            } else if (nowMonth === 11) {
+                this.$monthDate.text('DEC')
+            }
+    
+            }, 100)
+    }
+
+    progressLineContent() {
 
         setInterval(() => {
 
-            let mustDoNumber = mustDoCounter.textContent;
-            let doneTodoNumber = doneTodoCounter.textContent;
+            let mustDoNumber = this.$mustDoCounter.text();
+            let doneTodoNumber = this.$doneTodoCounter.text();
 
             const total = Number(mustDoNumber) + Number(doneTodoNumber);
             let percent = doneTodoNumber * 100 / total;
         
-            progressLine.style.width = `${percent}%`
+            this.$progressLine.css('width', `${percent}%`)
         }, 500)
-        
-        
     }
 }
